a0584314c787386991e5713079a09c78254e92e1
Kevin <KevinKYuen@gmail.com>
2014-10-19 12:12:42 -0400

    grabbing files based on the url provided in csv

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-response = urllib2.urlopen('http://www.cic.gc.ca/english/information/applications/guides/EG7TOC.asp')
-html = response.read()
-soup = BeautifulSoup(html)
+import csv
 
-print(soup.get_text())
+save_dir = "harvest_dir"
+url_file = 'docs.csv'
+
+f = open(url_file)
+csv_f = csv.reader(f)
+
+next(csv_f) #skip firstline
+for row in csv_f:
+
+	response = urllib2.urlopen(row[0])
+	html = response.read()
+	soup = BeautifulSoup(html)
 
 
+	title = soup.title.string
+	main = soup.find('main').get_text().encode('utf-8').strip()
+	print(main)
+	print(title)
+
+	#save to file
+	filename = save_dir + "/" + soup.title.string + ".txt"
+	
+	outfile = open(filename, 'w')
+	outfile.write(main)
+	outfile.close()
